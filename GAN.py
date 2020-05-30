3141f5dbf10ee4a79dd6913b904ef5f96e6c7d02
tanaymittal25 <tanay.mittal25@gmail.com>
2020-05-30 23:44:25 +0530

    Define Discriminator

--- GAN.py
+++ GAN.py
@@ -63,3 +63,23 @@ class G(nn.Module):
 #Create Generator
 netG = G()
 netG.apply(weights_init)
+
+#Define Discriminator
+class D(nn.Module):
+    def __init__(self):
+        super(D, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(3, 64, 4, 2, 1, bias = False),
+            nn.LeakyReLU(0.2, inplace = True),
+            nn.Conv2d(64, 128, 4, 2, 1, bias = False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace = True),
+            nn.Conv2d(128, 256, 4, 2, 1, bias = False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace = True),
+            nn.Conv2d(256, 512, 4, 2, 1, bias = False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace = True),
+            nn.Conv2d(512, 1, 4, 1, 0, bias = False),
+            nn.Sigmoid()
+        )
